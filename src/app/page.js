8714d4f6956c5327feb92ed9129d05c9da9d59c7
Christopher Darmawirya <christopher.fx45@gmail.com>
2024-05-09 20:59:20 +0900

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,203 @@
+"use client"
+
+import { Box, Center,Text } from "@chakra-ui/react";
+import Link from "next/link";
 import Image from "next/image";
+import { useState, useEffect } from "react";
+import LOGO from "../../public/LOGO.png"
+import IMG1 from "../../public/IMG1.jpg"
+import IMG2 from "../../public/IMG2.jpg"
+import IMG3 from "../../public/IMG3.jpg"
+import IMG4 from "../../public/IMG4.jpg"
+import IMG5 from "../../public/IMG5.jpg"
+import localfont from "next/font/local"
+
+
+const TNRfont = localfont({
+  src: [
+    {
+      path :"/TNR.woff2",
+      style: "normal",
+      weght : "400"
+    }
+
+]})
+const myFont = localfont({
+  src: [
+    {
+      path :"/Lucida.woff2",
+      style: "normal",
+      weght : "400"
+    }
+
+]})
 
 export default function Home() {
+  const [width, setWidth] = useState(0)
+  const [height, setHeight] = useState(0)
+
+  const handleWindowResize = () => {
+    setWidth(window.innerWidth * 0.98);
+    setHeight(window.innerHeight);
+  }
+  useEffect(() => {
+    // component is mounted and window is available
+    handleWindowResize();
+    window.addEventListener('resize', handleWindowResize);
+    // unsubscribe from the event on component unmount
+    return () => window.removeEventListener('resize', handleWindowResize);
+  }, []);
+  const Screens = ()=>{
+    if (width > 800){
+      let widthLogo = width/ 3.3
+      return(
+        <>
+        <Image src={IMG2} alt = "img2" style={{
+            position : "absolute",
+            top : 0,
+            left: 0 ,
+            minWidth : "10",
+            width : width/20,
+            height : "auto"
+          }}></Image>
+        <Image src={IMG3} alt = "img2" style={{
+            position : "absolute",
+            top : height/7,
+            right: width/70 ,
+            minWidth : "10",
+            width : width/20,
+            height : "auto"
+          }}></Image>
+        <Image src={IMG4} alt = "img3" style={{
+            position : "absolute",
+            top : height/1.2,
+            right: width/70 ,
+            minWidth : "10",
+            width : width/20,
+            height : "auto"
+          }}></Image>
+        <Image src={IMG5} alt = "img5" style={{
+            position : "absolute",
+            top : height/1.25,
+            left: 0 ,
+            minWidth : "10",
+            width : width/20,
+            height : "auto"
+          }}></Image>
+        <Center width={"100%"} padding={"3%"}  className= {myFont.className} style={{
+          color: "black",
+          fontSize: 24
+        }}>
+          <Center width={"95%"} flex={true}>
+          <p>Christine </p>
+          <Box padding={"1%"} w={"75%"}><hr style={{
+              display: "block",
+              height: "3px",
+              borderBlockColor: "black"
+
+
+          }}/></Box>
+          <p>Foodirectory.sg</p>
+
+          </Center>
+
+        </Center>
+        <Center marginBottom={"5%"}>
+
+          <Image src={LOGO} alt = "LOGO" style={{
+            width : widthLogo,
+            height : "auto"
+          }}></Image>
+        </Center>
+        <Image src={IMG1} alt = "img1" style={{
+            marginLeft: width/20,
+            minWidth : "10",
+            width : width/27,
+            height : "auto"
+          }}></Image>
+        <Box style={{ color : "black"}} paddingLeft={"5%"} paddingTop={"2%"} className={TNRfont.className} fontSize={"1.5vw"}>
+
+          Wandering gastronaut on a quest of unpretentious<br/>
+          food, hidden gems and exquisite staycation
+        </Box>
+        </>
+
+      )
+    }
+    else{
+      return(
+        <>
+        <Image src={IMG2} alt = "img2" style={{
+            padding : "2%",
+            minWidth : "10",
+            width : width/7,
+            height : "auto"
+          }}></Image>
+        <Image src={IMG1} alt = "img1" style={{
+            marginLeft: width/1.1,
+            minWidth : "10",
+            width : width/12,
+            height : "auto"
+          }}></Image>
+        <Center width={"100%"} padding={"3%"} flex={true} className= {myFont.className} style={{
+          color: "black",
+          fontSize:"3vw"
+        }}>
+
+          <p>Christine</p>         
+          <Box padding={"1%"} w={"75%"}><hr style={{
+              display: "block",
+              height: "3px",
+              borderBlockColor: "black"
+          }}/></Box>
+          <p>Foodirectory.sg</p>
+        </Center>
+        <Center>
+
+          <Image src={LOGO} alt = "LOGO" style={{
+            minWidth : "600",
+            width : width* 0.8,
+            height : "auto"
+          }}></Image>
+ 
+        </Center>
+        <Box wid style={{ color : "black"}} paddingLeft={"5%"} paddingTop={"15%"} className={TNRfont.className} fontSize={"2.5vw"}>
+                    Wandering gastronaut on a quest of unpretentious<br/>
+                    food, hidden gems and exquisite staycation
+        </Box>
+        <Image src={IMG1} alt = "img1" style={{
+            marginLeft: width * 0.1,
+            minWidth : "10",
+            width : width/12,
+            height : "auto",
+            paddingTop : height * 0.03
+          }}>
+
+          </Image>
+          <Image src={IMG4} alt = "img4" style={{
+            marginLeft: width * 0.8,
+            minWidth : "10",
+            width : width/8,
+            height : "auto",
+            paddingTop : height * 0.12
+          }}>
+
+          </Image>
+        </>
+
+      )
+    }
+
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main style={{backgroundColor:"#f6f1eb"}} >
+      
+      <Box w = {width} h= {height *1.1} backgroundColor="#f6f1eb">
+        <Box  w = {width * 0.98}><Screens/> </Box>
+         </Box>
+     
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
